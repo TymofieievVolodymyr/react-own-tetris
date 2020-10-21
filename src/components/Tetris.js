c6463c7b7785167adbc96f64d332c0dc05219902
Volodymyr <vovanium.timofeev@gmail.com>
2020-10-21 18:31:20 +0300

    added few utilities functions

---
+++ src/components/Tetris.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// Components
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+import {createStage} from "../gameHelpers";
+
+const Tetris = () => {
+    return (
+        <div>
+            <Stage stage={createStage()}/>
+            <aside>
+                <div>
+                    <Display text="Score" />
+                    <Display text="Rows" />
+                    <Display text="Level" />
+                </div>
+                <StartButton />
+            </aside>
+        </div>
+    );
+};
+
+export default Tetris;
