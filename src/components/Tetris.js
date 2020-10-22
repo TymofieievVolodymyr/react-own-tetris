3fb8a70792e2d1e1a2cc0326c8d25602e50229b0
Volodymyr <vovanium.timofeev@gmail.com>
2020-10-22 15:40:19 +0300

    added more logic in hooks and handle it in Tetris.js

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {createStage} from "../gameHelpers";
 
 // Components
 import Stage from './Stage';
@@ -11,17 +12,57 @@ import {usePlayer} from '../hooks/usePlayer'
 
 // Styled Components
 import {StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
+import {randomTetromino} from "../tetrominos";
 
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
+    // {
+    //     pos: { x: 0, y: 0 },
+    //     tetromino: randomTetromino().shape,
+    //     collided: false,
+    // }
     const [stage, setStage] = useStage(player);
+
+    // new 20 length [[12 length 0, clear]]
+    console.log(stage);
+
+    const movePlayer = dir => {
+        updatePlayerPos({x: dir, y: 0});
+    }
+
+    const startGame = () => {
+        console.log("test")
+        // Reset everything
+        setStage(createStage());
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({x: 0, y: 1, collided: false})
+    }
+
+    const dropPlayer = () => {
+        drop();
+    }
+
+    const move = ({keyCode}) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            }
+        }
+    }
 
 
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
                 <Stage stage={stage}/>
                 <aside>
@@ -34,7 +75,7 @@ const Tetris = () => {
                                 <Display text="Level"/>
                             </div>
                         )}
-                    <StartButton/>
+                    <StartButton callback={startGame}/>
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,37 @@
-import { useState } from 'react';
-import { createStage } from '../gameHelpers';
+import {useState, useEffect} from 'react';
+import {createStage} from '../gameHelpers';
 
-export const useStage = () => {
-  const [stage, setStage] = useState(createStage());
+export const useStage = (player, resetPayer) => {
+    const [stage, setStage] = useState(createStage());
 
-  return [stage, setStage];
+    useEffect(() => {
+        const updateStage = prevStage => {
+            //prevStage 20 length [[12 length 0, clear]]
+            // First flush the stage
+            const newStage = prevStage.map(row =>
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            );
+
+            // console.log(newStage);
+            // debugger;
+            // Then draw the tetromino
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        newStage[y + player.pos.y][x + player.pos.x] = [
+                            value,
+                            `${player.collided ? 'merged' : 'clear'}`,
+                        ];
+                    }
+                });
+            });
+
+            return newStage;
+        };
+
+        setStage(prev => updateStage(prev));
+    }, [player]);
+
+
+    return [stage, setStage];
 }
